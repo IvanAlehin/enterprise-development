7a88c417e238b9c81ef7004a92743a4092a6176a
IvanAlehin <ivanalehin236@gmail.com>
2025-11-25 15:20:18 +0400

    add Migration

---
+++ EstateAgency/EstateAgency.Infrastructure/Migrations/20251125150129_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstateAgency.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "counterparties",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    full_name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    passport_number = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    phone_number = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_counterparties", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "real_estates",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    type = table.Column<int>(type: "int", nullable: false),
+                    purpose = table.Column<int>(type: "int", nullable: false),
+                    cadastral_number = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    address = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    floors_total = table.Column<int>(type: "int", nullable: false),
+                    total_area = table.Column<double>(type: "double", nullable: false),
+                    rooms = table.Column<int>(type: "int", nullable: false),
+                    ceiling_height = table.Column<double>(type: "double", nullable: false),
+                    floor_number = table.Column<int>(type: "int", nullable: false),
+                    has_encumbrances = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_real_estates", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "applications",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    counterparty_id = table.Column<int>(type: "int", nullable: false),
+                    real_estate_id = table.Column<int>(type: "int", nullable: false),
+                    transaction_amount = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    type = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    date = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_applications", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_applications_counterparties_counterparty_id",
+                        column: x => x.counterparty_id,
+                        principalTable: "counterparties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_applications_real_estates_real_estate_id",
+                        column: x => x.real_estate_id,
+                        principalTable: "real_estates",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_applications_counterparty_id",
+                table: "applications",
+                column: "counterparty_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_applications_real_estate_id",
+                table: "applications",
+                column: "real_estate_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "applications");
+
+            migrationBuilder.DropTable(
+                name: "counterparties");
+
+            migrationBuilder.DropTable(
+                name: "real_estates");
+        }
+    }
+}
