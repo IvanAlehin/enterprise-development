27357d81f8c5dfc85a4379a3bf5f48bd009c6c9a
IvanAlehin <ivanalehin236@gmail.com>
2025-11-25 14:11:27 +0400

    add Api Configuration

--- EstateAgency/EstateAgency.Api/Program.cs
+++ EstateAgency/EstateAgency.Api/Program.cs
@@ -1,25 +1,61 @@
+using EstateAgency.Application.Contracts;
+using EstateAgency.Domain.Entities;
+using EstateAgency.Domain.Interfaces;
+using EstateAgency.Infrastructure.Persistence;
+using EstateAgency.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.AddServiceDefaults();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.UseInlineDefinitionsForEnums();
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+});
+
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 0))
+    ));
+
+builder.Services.AddScoped<IRepository<EstateAgency.Domain.Entities.Application>, DbRepository<EstateAgency.Domain.Entities.Application>>();
+builder.Services.AddScoped<IRepository<RealEstate>, DbRepository<RealEstate>>();
+builder.Services.AddScoped<IRepository<Counterparty>, DbRepository<Counterparty>>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+    db.Seed();
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Estate Agency");
+});
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
+app.MapDefaultEndpoints();
 app.MapControllers();
 
 app.Run();
